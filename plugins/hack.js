ba764e39600f7481473e44ad7dcc49f06dcb0923
DEXTER <144078621+purnapurna2007@users.noreply.github.com>
2024-05-11 02:24:10 +0530

    Add files via upload

--- plugins/hack.js
+++ plugins/hack.js
@@ -1,25 +1,20 @@
 const { smd, sleep } = require('../lib');
 
-smd({ cmdname: "hack", type: "fun", info: "hacking prank", filename: __filename }, async (citel) => {
+smd({ cmdname: "developer", type: "fun", info: "𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥", filename: __filename }, async (citel) => {
   const messages = [
-    "Injecting Malware",
-    " █ 10%",
-    " █ █ 20%",
-    " █ █ █ 30%",
-    " █ █ █ █ 40%",
+    "𝗥𝗖𝗗 𝗧𝗘𝗔𝗠 𝗠𝗘𝗠𝗕𝗘𝗥𝗦 ",
+    "` 𝗥𝗘𝗔𝗟 𝗗𝗘𝗫𝗧𝗘𝗥` 94789958225 ",
+    " `𝗖𝗬𝗕𝗘𝗥 𝗗𝗘𝗫𝗧𝗘𝗥` 94785274495 ",
+    " `𝗞𝗜𝗡𝗚 𝗥𝗔𝗩𝗜` 94757660788 ",
+    " `𝗖𝗛𝗔𝗞𝗬 𝗕𝗢𝗬` 94741698653 ",
     " █ █ █ █ █ 50%",
     " █ █ █ █ █ █ 60%",
     " █ █ █ █ █ █ █ 70%",
     " █ █ █ █ █ █ █ █ 80%",
     " █ █ █ █ █ █ █ █ █ 90%",
     " █ █ █ █ █ █ █ █ █ █ 100%",
-    "System hyjacking on process.. \\n Conecting to Server error to find 404 ",
-    "Device successfully connected... \\n Riciving data...",
-    "Data hyjacked from divice 100% completed \\n killing all evidence killing all malwares...",
-    " HACKING COMPLETED ",
-    " SENDING LOG DOCUMENTS...",
-    " SUCCESSFULLY SENT DATA AND Connection disconnected",
-    "BACKLOGS CLEARED"
+    "*RCD BOT PREFECT*",
+    "*THANKS FOR ALL*"
   ];
 
   let editedMessage;
